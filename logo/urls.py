8192bf52dcf070cb8ec67dd96a74f5047f4b3132
Mahmudul Alam <mahmudula2000@gmail.com>
2021-02-21 15:29:56 +0600

    added privacy policy link

--- logo/urls.py
+++ logo/urls.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('privacy-policy/',
+         lambda x: HttpResponse("Privacy and policy will be added as soon as it goes to production")),
     path('chat/', include('chat.urls')),
 ]
 
