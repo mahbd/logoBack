1b376c750d23b64d303c9ce1130cc0bb894097ef
Mahmudul Alam <mahmudula2000@gmail.com>
2021-02-21 15:19:25 +0600

    added facebook messenger

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('messenger/', views.messenger),
     path('<str:room_name>/', views.room, name='room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -5,3 +5,7 @@ def room(request, room_name):
     return render(request, 'chat/chat.html', {
         'room_name': room_name
     })
+
+
+def messenger(request):
+    return render(request, 'chat/messenger.html')
