cf17027b358eccb1597458aa36707a1db047e680
Ubuntu <mahmuduly2000@gmail.com>
2021-02-27 01:00:05 +0000

    added location variable

--- logo/wsgi.py
+++ logo/wsgi.py
@@ -7,7 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append(os.path.abspath('..'))
 
 from django.core.wsgi import get_wsgi_application
 
