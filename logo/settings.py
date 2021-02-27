d33d59bf49dc55208970fa6ae3019dbd058ecb0f
Mahmudul Alam <mahmudula2000@gmail.com>
2021-02-27 07:11:29 +0600

    added postgres server

--- logo/settings.py
+++ logo/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'logo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'logo',
+        'USER': 'mah',
+        'PASSWORD': '1234',
+        'HOST': '20.84.106.114',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
